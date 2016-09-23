279a49c46667ff03710764b297495398690e9760
JKasyan <j.kasyan86@gmail.com>
2016-09-23 13:04:53 +0300

    pg

--- postgres/app.js
+++ postgres/app.js
@@ -4,18 +4,33 @@
 var pg = require('pg');
 var fs = require('fs');
 
-//TODO: Read file private
-var connectionString = null;
-var client = new pg.Client(connectionString);
-client.connect();
+fs.access('private', function(err){
+    if(err) throw 'File "private" not exists!';
+    fs.readFile('private','utf8', function(err, data){
+        if(err) throw err;
+        if(!data) throw 'File "Private" is empty'
+        var url = parseDataToURL(data);
+        var client = new pg.Client(url);
+        client.connect();
 
-var query = client.query('SELECT * FROM Book');
+        var query = client.query('SELECT count(*) FROM author');
 
-query.on('row', function(row){
-    console.log(row);
-})
+        query.on('row', function(row){
+            console.log(row);
+        })
 
-query.on('end', function() {
-    console.log('end');
-    client.end();
-})
+        query.on('end', function() {
+            console.log('end');
+            client.end();
+        })
+    })
+});
+
+
+function parseDataToURL(data) {
+    var dbData = JSON.parse(data);
+    console.log('dbData = ', dbData);
+    var url = 'tcp://' + dbData.user + ':' + dbData.password + '@' + dbData.host + ':' + dbData.port + '/' + dbData.database;
+    url += '?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory';
+    return url;
+}
