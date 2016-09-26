55a5d1745692b8022fed11c8c4dcb2c02628665d
JKasyan <j.kasyan86@gmail.com>
2016-09-26 18:32:12 +0300

    mongoose + connect

---
+++ connect/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by 1 on 9/26/2016.
+ */
+var connect = require('connect');
+connect()
+    .use(logger)
+    .use('/hello', hello)
+    .listen(3000);
+
+function logger(req, res, next) {
+    console.log('%s %s', req.method, req.url);
+    next();
+}
+
+function hello(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Hello!');
+}
+
+function restrict(req, res, next) {
+    var authorization = req.headers.authorization;
+    if(!authorization) next(new Error('Unauthorized'));
+    var parts = authorization.split(' ');
+    var scheme = parts[0];
+    var auth = new Buffer(parts[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    next();
+}
--- mongoose/app.js
+++ mongoose/app.js
@@ -5,41 +5,53 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 var points = null;
 
-fs.readFile('private','utf8', function (err, data) {
+fs.readFile('private', 'utf8', function (err, data) {
     console.log(data);
-    if(err) throw err;
+    if (err) throw err;
     mongoose.connect(data);
     var db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'Connection error!'));
-    db.once('open', function() {
+    db.once('open', function () {
         console.log('Open connection...');
         var Schema = mongoose.Schema;
         var PointSchema = new Schema({
-            lat:Number,
-            lng:Number,
+            lat: Number,
+            lng: Number,
             speed: Number,
             bearing: Number,
             altitude: Number,
             batt: Number,
-            _id:{
-                $oid:String
+            _id: {
+                $oid: String
             },
-            id:String
-        }, {collection:'Point'});
+            id: String
+        }, {collection: 'Point'});
         var point = mongoose.model('point', PointSchema);
         point.where('timestamp')
             .gt(new Date(2016, 7, 1).getTime() / 1000)
-            .select('speed')
-            .limit(10)
+            .select('lat')
+            .select('lng')
+            .limit(100)
             .exec(function (err, result) {
-            if(err) throw err;
-            console.log('Size = ' + result.length);
-            for(var i = 0;i < result.length;i++) {
-                console.log('' + result[i]);
-            }
-            mongoose.disconnect();
-        });
+                if (err) throw err;
+                console.log('Size = ' + result.length);
+                var lat, lng;
+                var res = result.reduce(function (res, current) {
+                    if (!lat && !lng) {
+                        lat = current.lat;
+                        lng = current.lng;
+                        return 0;
+                    }
+                    var x = current.lat - lat;
+                    var y = current.lng - lng;
+                    lat = current.lat;
+                    lng = current.lng;
+                    return res + Math.sqrt(x * x + y * y);
+                }, 0);
+                console.log(res);
+                mongoose.disconnect();
+            });
     });
 });
 
