dd84d1e8f2239c6f3c2e4b6ca0173abd74f6446b
JKasyan <j.kasyan86@gmail.com>
2016-10-05 18:57:09 +0300

    chat

--- socket_mongo/app.js
+++ socket_mongo/app.js
@@ -2,13 +2,14 @@
  * Created by 1 on 10/3/2016.
  */
 
-var fs = require('fs');
 var io = require('socket.io')(3003);
 
 var express = require('express');
 var app = express();
 
-var s = new Set();
+//socket -> name
+var sockets = {};
+var quantity = 1;
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
@@ -19,18 +20,34 @@ app.listen(3000);
 io.on('connect', function(socket) {
 
     console.log('connected!');
+    console.log('sockets = ', sockets);
+    var newName = 'Guest' + quantity++;
+    socket.emit('newName', {name: newName});
+    socket.hello = 'world';
 
     socket.join('generalRoom');
+    socket.emit('users', sockets);
+    sockets[socket.id] = newName;
 
-    //broadcast.to(room).emit
-    socket.broadcast.to('generalRoom').emit('newUser', {socketId: socket.id});
+    //var clients = io.sockets.adapter.rooms['generalRoom'].sockets;
+    //console.log('Clients = ', clients);
+
+    //broadcast to all
+    socket.broadcast.to('generalRoom').emit('newUser', {socketId: socket.id, name: newName});
+    //and to new user all users
 
     socket.on('msgForAll', function(data) {
         socket.to('myRoom').emit('broadcast', data);
     });
 
     socket.on('sendToUser', function (data) {
-        console.log('sendToUser = ' + data.id + ', message = ' + data.message);
-        io.to(data.id).emit('fromUser', data.message);
+        console.log('sendToUser = ' + sockets[data.id] + ', message = ' + data.message);
+        io.to(data.id).emit('fromUser', {message:data.message, sender:data.id});
+    });
+
+    socket.on('disconnect', function () {
+        console.log('disconnect = ' + socket.id);
+        delete sockets[socket.id];
+        socket.broadcast.to('generalRoom').emit('disconnectUser', {socketId: socket.id});
     });
 });
